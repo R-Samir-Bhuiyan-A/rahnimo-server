8043d557402ada4689e15707de65aacd0bbc163c
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2025-12-23 20:55:32 +0600

    users routes controller

--- routes/adminRoutes/index.js
+++ routes/adminRoutes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./adminAuthRoutes.js"
+import adminUsersRoutes from "./adminUsersRoutes.js"
 
 const router = express.Router()
 
 router.use("/auth", authRoutes)
+router.use("/users", adminUsersRoutes)
 
 export default router
