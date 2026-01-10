e4d5462e18d45886fe7da7423253f2480a1c09f6
Tuhin Al Rakib <eng.tuhin77@gmail.com>
2026-01-10 22:19:29 +0600

    caching modify

--- cachingFunction/teamCaching.js
+++ cachingFunction/teamCaching.js
@@ -3,14 +3,14 @@ import redisClient from "../config/redis.js";
 
 // 🔹 Get team from cache
 export const getTeamsCache = async (key) => {
-  const data = await redisClient.get(`teams:${key}`);
+  const data = await redisClient.get(`rahnimoTeams:${key}`);
   return data ? JSON.parse(data) : null;
 };
 
 // 🔹 Set Team cache
 export const setTeamsCache = async (key, value, ttl = 3600) => {
   await redisClient.set(
-    `teams:${key}`,
+    `rahnimoTeams:${key}`,
     JSON.stringify(value),
     "EX",
     ttl 
@@ -18,26 +18,26 @@ export const setTeamsCache = async (key, value, ttl = 3600) => {
 };
 
 
+// 🔹 Delete ALL projects cache
+export const deleteTeamsCache = async () => {
+  const keys = await redisClient.keys("rahnimoTeams:*");
+  if (keys.length) await redisClient.del(keys);
+};
+
 export const getTeamByIdCache = async (id) => {
-  const data = await redisClient.get(`team:${id}`);
+  const data = await redisClient.get(`rahnimoTeam:${id}`);
   return data ? JSON.parse(data) : null;
 };
 
 export const setTeamByIdCache = async (id, project, ttl = 3600) => {
   await redisClient.set(
-    `team:${id}`,
+    `rahnimoTeam:${id}`,
     JSON.stringify(project),
     "EX",
     ttl 
   );
 };
 
-// 🔹 Delete ALL projects cache
-export const deleteTeamsCache = async () => {
-  const keys = await redisClient.keys("teams:*");
-  if (keys.length) await redisClient.del(keys);
-};
-
 export const deleteTeamByIdCache = async (id) => {
-  await redisClient.del(`team:${id}`);
+  await redisClient.del(`rahnimoTeam:${id}`);
 };
--- cachingFunction/tokenCaching.js
+++ cachingFunction/tokenCaching.js
@@ -4,22 +4,22 @@ const REFRESH_TTL = 7 * 24 * 60 * 60;
 
 // ✅ Save refresh token (token -> userId)
 export const setRefreshTokenCache  = async (token, userId) => {
-    return redisClient.set(`refresh:${token}`, userId, "EX", REFRESH_TTL)
+    return redisClient.set(`rahnimoRefresh:${token}`, userId, "EX", REFRESH_TTL)
 }
 
 // ✅ Get userId using refresh token
 export const getRefreshTokenCache = async(token)=>{
-    const userId =  redisClient.get(`refresh:${token}`)
+    const userId =  redisClient.get(`rahnimoRefresh:${token}`)
     return userId || null
 }
 
 export const deleteRefreshTokenCache = async(token)=>{
-    await redisClient.del(`refresh:${token}`)
+    await redisClient.del(`rahnimoRefresh:${token}`)
 }
 
 // Optional: Delete all tokens for a user (if needed)
 export const deleteAllRefreshTokensByUser = async (userId) => {
-  const keys = await redisClient.keys(`refresh:*`);
+  const keys = await redisClient.keys(`rahnimoRefresh:*`);
 
   for (const key of keys) {
     const storedUser = await redisClient.get(key);
